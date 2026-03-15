6e08e5e4faa5d27c61681715f97268904f361302
Mzwandile <bongwe@users.noreply.github.com>
2026-03-15 19:41:38 +0200

    Initial commit

---
+++ src/main/java/za/co/obomvu/interactive/Morabaraba/MorabarabaApplication.java
@@ -0,0 +1,13 @@
+package za.co.obomvu.interactive.Morabaraba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorabarabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorabarabaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/obomvu/interactive/Morabaraba/MorabarabaApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.obomvu.interactive.Morabaraba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorabarabaApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}

f68e32ff67ceb0ff94d9f48aaed8039d68f8bc16
mzwandile.mahlangu <obomvuinteractive@gmail.com>
2026-04-12 12:04:28 +0200

    use in memory database

--- src/main/java/za/co/obomvu/interactive/Morabaraba/config/DatabaseConfig.java
+++ src/main/java/za/co/obomvu/interactive/Morabaraba/config/DatabaseConfig.java
@@ -3,22 +3,9 @@ package za.co.obomvu.interactive.Morabaraba.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/morabaraba");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("Bongwe");
-        return dataSource;
-    }
 
     @Bean
     public ObjectMapper objectMapper() {

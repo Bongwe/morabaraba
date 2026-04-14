7bd568bea9d9837f16d5ad0308c099d38cdf97b9
mzwandile.mahlangu <obomvuinteractive@gmail.com>
2026-04-14 23:19:32 +0200

    java app build updates

--- src/main/java/za/co/obomvu/interactive/Morabaraba/controller/GlobalExceptionHandler.java
+++ src/main/java/za/co/obomvu/interactive/Morabaraba/controller/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package za.co.obomvu.interactive.Morabaraba.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,6 +20,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, String>> handleRuntime(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ResponseEntity<Void> handleMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException e) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
     }
 
     @ExceptionHandler(Exception.class)
